5d122d7590b3813a03db7b7f13660476939905df
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-11-27 10:10:34 +0300

    talking bot`

---
+++ settings/variables_area.py
@@ -0,0 +1,10 @@
+import configparser
+import os
+
+database=os.getenv('database')
+user=os.getenv('user')
+host=os.getenv('host')
+password=os.getenv('password')
+port=os.getenv('port')
+token=os.getenv('token')
+
